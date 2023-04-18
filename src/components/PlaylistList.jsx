aeeb75a7ac2c6866c7f3d5980d4c29dbb2331f38
sterlingh78 <sterlingh78@yahoo.com>
2023-04-17 19:07:01 -0600

    added unfollow functionality and UI

--- src/components/PlaylistList.jsx
+++ src/components/PlaylistList.jsx
@@ -1,10 +1,41 @@
+import { useState } from 'react';
 import AddModal from './AddModal';
+import '../App.css';
 export default function PlaylistList({
 	handlePlaylist,
 	ownedPlaylists,
 	user,
 	getPlaylists,
 }) {
+	const [hovered, setHovered] = useState(null);
+	const handleMouseOver = (index) => {
+		setHovered(index);
+	};
+	const handleMouseLeave = () => {
+		setHovered(null);
+	};
+	const handleUnfollow = async (index) => {
+		console.log(ownedPlaylists[index]);
+
+		try {
+			const response = await fetch(
+				`https://api.spotify.com/v1/playlists/${ownedPlaylists[index].id}/followers`,
+				{
+					headers: {
+						Authorization: 'Bearer ' + localStorage.getItem('access-token'),
+					},
+					method: 'DELETE',
+				}
+			);
+			const string = await response.text();
+			const json = string === '' ? {} : JSON.parse(string);
+			console.log('delete test', json);
+			getPlaylists(user);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+	console.log('hovered', hovered);
 	return (
 		<div>
 			<AddModal
@@ -27,13 +58,46 @@ export default function PlaylistList({
 							</label>
 							{ownedPlaylists.map((playlist, i) => {
 								return (
-									<button
+									<div
 										key={i}
-										onClick={() => handlePlaylist(i)}
-										className='btn btn-secondary'
+										className=' flex'
+										onMouseOver={() => handleMouseOver(i)}
+										onMouseLeave={handleMouseLeave}
 									>
-										{playlist.name}
-									</button>
+										<button
+											onClick={() => handlePlaylist(i)}
+											className={`btn btn-secondary ${
+												hovered === i ? 'w-5/6' : 'w-full'
+											}`}
+										>
+											{playlist.name}
+										</button>
+										<button
+											onClick={() => handleUnfollow(i)}
+											className={`${
+												hovered === i ? 'w-1/6' : 'hidden'
+											} btn btn-primary ml-1`}
+										>
+											<svg
+												xmlns='http://www.w3.org/2000/svg'
+												width='24'
+												height='24'
+												viewBox='0 0 24 24'
+												fill='none'
+												stroke='#000000'
+												strokeWidth='2'
+												strokeLinecap='round'
+												strokeLinejoin='round'
+											>
+												<line
+													x1='5'
+													y1='12'
+													x2='19'
+													y2='12'
+												></line>
+											</svg>
+										</button>
+									</div>
 								);
 							})}
 						</div>
